2c9dcb37565c0e5bc2fc4c11211823f2eba9cf74
Eugene Kusnetsov <phpsql.net@gmail.com>
2022-06-28 18:42:34 +0300

    Init commit

---
+++ js/script.js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.slider').slick({ 
+        arrows:false,
+        dots:true,
+        adaptiveHeigh:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:900,
+        easing:'ease',
+        infinite:false,
+        autoplaySpeed:1500,
+        autoplay: true,
+        infinite: false,
+        autoplaySpeed:3000,
+    });    
+});
